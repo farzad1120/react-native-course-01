b1cdcfb1dba778d7aa81a9cbc2ee500e93c57a2a
Farzad Zarasvand <dev.farzadz@gmail.com>
2020-02-17 01:48:03 +0330

    add flatlist and useState

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {uuid} from 'uuidv4';
+
 import Header from './components/Header';
+
 const App = () => {
+  const [items, setItem] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
