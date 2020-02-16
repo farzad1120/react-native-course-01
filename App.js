be820acaf9ff9ec6910d9e9ec8c784a5dcace94d
Farzad Zarasvand <dev.farzadz@gmail.com>
2020-02-17 02:37:00 +0330

    add AddItem component

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {View, StyleSheet, FlatList, Alert} from 'react-native';
 import {uuid} from 'uuidv4';
 
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
   const [items, setItems] = useState([
@@ -17,9 +18,17 @@ const App = () => {
     setItems(prevItems => prevItems.filter(item => item.id !== id));
   };
 
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item');
+    } else {
+      setItems(prevItems => [{id: uuid(), text}, ...prevItems]);
+    }
+  };
   return (
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
         renderItem={({item}) => (
